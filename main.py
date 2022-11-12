b6adc05e8cc2ab5155a4ffdc6198795894914c52
ajzliu <42823422+ajzliu@users.noreply.github.com>
2022-11-12 15:56:10 -0800

    Add initial files + outline

---
+++ main.py
@@ -0,0 +1,13 @@
+from typing import Union
+from fastapi import FastAPI
+
+app = FastAPI()
+
+# TODO:
+# Endpoint for a registration form submission (with spam prevention with Cloudflare Turnstile)
+#  - Adds a new row to Baserow
+#  - Should also send a verification email using Postmark
+# Endpoint for email verification
+#  - Updates Baserow with confirmation that the email has been sent
+# Endpoint for NCES school search for form submission
+# Endpoint to get data from Baserow using a registrant's ID
