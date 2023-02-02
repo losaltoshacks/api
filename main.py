eb39af48f197c56b2dee2b518d2beb4117fcc0eb
ajzliu <42823422+ajzliu@users.noreply.github.com>
2023-02-01 19:57:08 -0800

    Remove unused endpoints

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import imp
 from fastapi import FastAPI
-from routers import register, school_search, verify, attendees
+from routers import register, verify, attendees
 from auth import auth_handler
 from dotenv import load_dotenv
 
@@ -8,18 +7,7 @@ load_dotenv(override=True)
 
 app = FastAPI()
 
-app.include_router(register.router)
 app.include_router(auth_handler.router)
-app.include_router(school_search.router)
+app.include_router(register.router)
 app.include_router(attendees.router)
 app.include_router(verify.router)
-
-# TODO:
-# Endpoint for a registration form submission (with spam prevention with Cloudflare Turnstile)
-#  - Adds a new row to Baserow
-#  - Should also send a verification email using Postmark
-# Endpoint for email verification
-#  - Updates Baserow with confirmation that the email has been sent
-# Endpoint for NCES school search for form submission
-# Endpoint to get data from Baserow using a registrant's ID
-
