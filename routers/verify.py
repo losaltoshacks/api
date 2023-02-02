01866eb6e982c2e225829349f9b991774202e8af
ajzliu <42823422+ajzliu@users.noreply.github.com>
2023-02-01 20:35:46 -0800

    Fix formatting

--- routers/verify.py
+++ routers/verify.py
@@ -3,30 +3,30 @@ from pyairtable.api.table import Table
 from dependencies import get_table
 from auth.auth_handler import verify_jwt, decode_jwt
 
-router = APIRouter(
-    prefix="/verify",
-    tags=["verify"]
-)
+router = APIRouter(prefix="/verify", tags=["verify"])
+
 
 @router.get("/{token}")
-async def verify__email(token: str, table: Table = Depends(get_table)):
+async def verify_email(token: str, table: Table = Depends(get_table)):
     # token is invalid or expired
     if not verify_jwt(token):
-        raise HTTPException(status_code=403, detail="Invalid or expired verification code.")
-    
+        raise HTTPException(
+            status_code=403, detail="Invalid or expired verification link."
+        )
+
     payload = decode_jwt(token)
     attendee_id = payload["id"]
     email_type = payload["type"]
 
     if not attendee_id or not email_type:
         # token does not contain payload id
-        raise HTTPException(status_code=403, detail="No payload id:: Invalid verification code.")
-    
+        raise HTTPException(status_code=403, detail="Invalid verification link.")
+
     field_name = "Parent Email Verified" if email_type == "parent" else "Email Verified"
 
     try:
-        table.update(attendee_id, {field_name : True})
-        return {"Email has successfully been verified!"}
+        table.update(attendee_id, {field_name: True})
+        return "Email has successfully been verified!"
     except:
         # attendee does not exist
-        raise HTTPException(status_code=403, detail="Invalid verification code.")
+        raise HTTPException(status_code=403, detail="Invalid verification link.")
