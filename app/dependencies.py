f8b91bf42a8fa03b254a54ca67acfed5998f88e6
ajzliu <42823422+ajzliu@users.noreply.github.com>
2023-02-02 01:17:36 -0800

    Load from .env

--- app/dependencies.py
+++ app/dependencies.py
@@ -1,10 +1,14 @@
 from pyairtable.api.table import Table
 import os
+from dotenv import load_dotenv
+
 
 async def get_table():
-    AIRTABLE_KEY = os.getenv('AIRTABLE_KEY', default="")
-    BASE = os.getenv('BASE', default="")
-    TABLE = os.getenv('TABLE', default="")
+    load_dotenv()
+
+    AIRTABLE_KEY = os.getenv("AIRTABLE_KEY", default="")
+    BASE = os.getenv("BASE", default="")
+    TABLE = os.getenv("TABLE", default="")
 
     table = Table(AIRTABLE_KEY, BASE, TABLE)
 
