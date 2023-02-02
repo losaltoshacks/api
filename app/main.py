d3f24e916c18f43403bc42352f77592a83dc3c79
ajzliu <42823422+ajzliu@users.noreply.github.com>
2023-02-02 00:16:05 -0800

    Add CORSMiddleware

--- app/main.py
+++ app/main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from .routers import register, verify, attendees
 from .auth import auth_handler
 from dotenv import load_dotenv
@@ -6,6 +7,21 @@ from dotenv import load_dotenv
 load_dotenv(override=True)
 
 app = FastAPI()
+
+origins = [
+    "http://registration.losaltoshacks.com",
+    "https://registration.losaltoshacks.com",
+    "http://localhost",
+    "http://localhost:8000",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 app.include_router(auth_handler.router)
 app.include_router(register.router)
