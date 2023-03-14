5955cccd62489eaeb2061ca9827d7502550894e0
Sabrina <59457991+syenko@users.noreply.github.com>
2023-03-13 21:14:18 -0700

    Add endpoints for specific attendee properties

--- app/routers/mobile.py
+++ app/routers/mobile.py
@@ -28,3 +28,21 @@ async def add_attendee(attendee: MobileAttendee, table: Table = Depends(get_mobi
     # TODO: add input verification?
     res = table.create(attendee.getAirtableFields())
     return res
+
+# get specific attendee
+@router.get("/{attendee_id}")
+async def get_attendee(attendee_id: str, table: Table = Depends(get_mobile_table)):
+    return recordToMobileAttendee(table.get(attendee_id))
+
+# get attendee properties
+@router.get("/{attendee_id}/signed_in")
+async def check_attendee_signed_in(attendee_id: str, table: Table = Depends(get_mobile_table)):
+    return recordToMobileAttendee(table.get(attendee_id)).signed_in
+
+@router.get("/{attendee_id}/meals")
+async def get_attendee_meals(attendee_id: str, table: Table = Depends(get_mobile_table)):
+    return recordToMobileAttendee(table.get(attendee_id)).meals
+
+@router.get("/{attendee_id}/meals/{meal}")
+async def check_attendee_meal(attendee_id: str, meal: str, table: Table = Depends(get_mobile_table)):
+    return meal in recordToMobileAttendee(table.get(attendee_id)).meals
