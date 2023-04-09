8fb6c9168da949afca96637898cb16d1878ae1d0
Sabrina <59457991+syenko@users.noreply.github.com>
2023-04-09 03:25:01 -0700

    Add dietary restrictions to mobile attendee

--- app/models/mobile_attendee.py
+++ app/models/mobile_attendee.py
@@ -6,6 +6,8 @@ class MobileAttendee(BaseModel):
     meals: list[str] = Field(default=[])
     checked_in: bool = Field(default=False)
     name: str
+    dietary: list[str] = Field(default=[])
+    dietary_other: str = Field(default="")
 
     def getAirtableFields(self):
         return self.dict(
@@ -18,5 +20,7 @@ def recordToMobileAttendee(airtableRecord):
         uuid=fields["UUID"],
         meals=(fields["meals"] if "meals" in fields.keys() else []),
         checked_in=(fields["checked_in"] if "checked_in" in fields.keys() else False),
-        name=fields["Name"]
+        name=fields["Name"],
+        dietary=(fields["Dietary Restrictions"] if "Dietary Restrictions" in fields.keys() else []),
+        dietary_other=(fields["Dietary Other"] if "Dietary Other" in fields.keys() else "")
     )
